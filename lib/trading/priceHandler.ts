224cb0853ba02ab9445bb0b2856fd7a2ab37cb7c
lucas espinosa <email@lucasespinosa.com>
2025-08-21 18:05:36 -0300

    Add CSV export functionality for price drop heatmap and detailed trade analysis; update .gitignore to include CSV files

--- lib/trading/priceHandler.ts
+++ lib/trading/priceHandler.ts
@@ -3,33 +3,12 @@ import { polymarketAPILogger } from "../../utils/logger";
 
 
 
-const priceHandler = (price: number, tradeData: TradeData): number => {
-  const fixedPrice = Number(price.toFixed(2));
-  if (fixedPrice !== price) {
-    price = fixedPrice;
-  }
-
+const priceHandler = (tradeData: TradeData): number => {
   // Check if current time has 40 minutes or less (e.g., 8:40, 12:30, etc.)
-  const currentTime = new Date();
-  const minutes = currentTime.getMinutes();
-
-
-  if ([0.96, 0.97, 0.98, 0.99].includes(price)) {
-    price = 0.95;
-  }
-
-  if ([0.90, 0.91, 0.92, 0.93, 0.94].includes(price)) {
-    price = 0.90;
-  }
-
-  if (minutes <= 50) {
-    price = 0.80;
-    if (tradeData.title.includes("XRP")) {
-      polymarketAPILogger.info("XRP detected, setting price to 0.50");
-      price = 0.50;
-    }
-
-    polymarketAPILogger.info("Time has {minutes} minutes (≤50), setting price to 0.80", { minutes });
+  let price = 0.8
+  if (tradeData.title.includes("XRP") && tradeData.outcome.includes("Up")) {
+    polymarketAPILogger.info("XRP detected, setting price to 0.70");
+    price = 0.70;
   }
   return price;
 };
